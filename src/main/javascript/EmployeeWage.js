37f128d480f156160f0608858138a21dda3973fe
Atharva <atharvasharma.cs25@gmail.com>
2025-03-04 12:46:50 +0530

    [Atharva] Add. implemented functionality to calculate total wages for a month

--- src/main/javascript/EmployeeWage.js
+++ src/main/javascript/EmployeeWage.js
@@ -4,6 +4,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const WORKING_DAYS_PER_MONTH = 20;
 
 // Generating work type :- Absent, Part-Time, Full-Time
 // Function to get work hours
@@ -17,9 +18,13 @@ function getWorkHours(empCheck) {
             return 0;
     }
 }
-// Generating work type :- Absent, Part-Time, Full-Time
-let empCheck = Math.floor(Math.random() * 3);
-let empHours = getWorkHours(empCheck);
-let dailyWage = empHours * WAGE_PER_HOUR;
+// Calculating total monthly wage
+let totalEmpHours = 0;
 
-console.log(`Employee worked ${empHours} hours, Daily Wage: $${dailyWage}`);
+for (let day = 0; day < WORKING_DAYS_PER_MONTH; day++) {
+    let empCheck = Math.floor(Math.random() * 3);
+    totalEmpHours += getWorkHours(empCheck);
+}
+
+let totalWage = totalEmpHours * WAGE_PER_HOUR;
+console.log(`Total Hours Worked: ${totalEmpHours}, Monthly Wage: $${totalWage}`);
