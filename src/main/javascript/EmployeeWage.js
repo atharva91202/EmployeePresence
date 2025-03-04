0922957512bd54c69d7bf11316f020c4cd2bbb0a
Atharva <atharvasharma.cs25@gmail.com>
2025-03-04 12:38:33 +0530

    [Atharva] Refactored code to use function for determining work hours

--- src/main/javascript/EmployeeWage.js
+++ src/main/javascript/EmployeeWage.js
@@ -6,20 +6,20 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
 // Generating work type :- Absent, Part-Time, Full-Time
-let empCheck = Math.floor(Math.random() * 3);
-
-let empHours = 0;
-
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHours = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHours = FULL_TIME_HOURS;
-        break;
-    default:
-        empHours = 0;
+// Function to get work hours
+function getWorkHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-
+// Generating work type :- Absent, Part-Time, Full-Time
+let empCheck = Math.floor(Math.random() * 3);
+let empHours = getWorkHours(empCheck);
 let dailyWage = empHours * WAGE_PER_HOUR;
+
 console.log(`Employee worked ${empHours} hours, Daily Wage: $${dailyWage}`);
